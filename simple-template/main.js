e3db7d9518263e6a218fda80710a81e60ab42be0
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-08-22 11:01:11 +0200

    simple template with html out

--- simple-template/main.js
+++ simple-template/main.js
@@ -1,15 +1,30 @@
 
-
 class MyParagraph extends HTMLElement {
   constructor() {
     super();
 
-    const template = document.getElementById('my-paragraph')
-    const templateContent = template.content
+    // const template = document.getElementById('my-paragraph')
+    // const templateContent = template.content
+
+    const template = document.createElement('template')
+    template.innerHTML = this.getTemplateString()
 
     const shadowRoot = this.attachShadow({ mode: 'open' })
+    // shadowRoot.appendChild(templateContent.cloneNode(true))
+    shadowRoot.appendChild(document.importNode(template.content, true))
+  }
 
-    shadowRoot.appendChild(templateContent.cloneNode(true))
+  getTemplateString() {
+    return `
+      <style>
+        p {
+          color: gray
+        }
+      </style>
+      <p>
+        <slot name="my-text"></slot>
+      </p>
+    `
   }
 }
 
