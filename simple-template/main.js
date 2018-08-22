e635796c3dc256d2912599d6ef944ddb43b3abbf
Giorgio de la Barrera <giodelabarrera@gmail.com>
2018-08-22 10:58:16 +0200

    simple template with template in html

---
+++ simple-template/main.js
@@ -0,0 +1,16 @@
+
+
+class MyParagraph extends HTMLElement {
+  constructor() {
+    super();
+
+    const template = document.getElementById('my-paragraph')
+    const templateContent = template.content
+
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+
+    shadowRoot.appendChild(templateContent.cloneNode(true))
+  }
+}
+
+customElements.define('my-paragraph', MyParagraph)
